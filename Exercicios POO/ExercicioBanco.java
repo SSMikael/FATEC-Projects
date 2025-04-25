4c3dd5110cd94ecaf77106a07c7b600b27136468
Mikael S <150168560+SSMikael@users.noreply.github.com>
2025-04-25 02:32:28 +0000

    Criação da classe ContaBanco e suas variaveis, metodos e metodos especiais

---
+++ Exercicios POO/ExercicioBanco.java
@@ -0,0 +1,90 @@
+//Criar uma classe banco com os atributos numero da conta, tipo, nome do dono, saldo, status e os 
+// metodos abrir conta, fechar conta, depositar, sacar e pagar mensalidade.
+// criar dois clientes.
+
+// (Conta poupança ou conta corrente) (Não pode fechar conta com saldo negativo ou positivo) 
+// (abrir conta corrente ganha bonificação de R$ 50,00 e poupança R$ 150,00) (usuario 1 deposita R$ 300 e usuario 2 R$ 500)
+// (saque somente com saldo positivo) (Conta corrente paga 12 reais de mensalidade e poupança 20 reais)
+
+class ContaBanco {
+
+    private int numeroConta;
+    protected String tipo;
+    private String nomeDono;
+    private boolean status;
+    private double saldo;
+
+    // Inicializando as variáveis.
+    public ContaBanco(int numeroConta, String tipo, String nomeDono, double saldo, boolean status) {
+
+        this.numeroConta = numeroConta;
+        this.tipo = tipo;
+        this.nomeDono = nomeDono;
+        this.saldo = saldo;
+        this.status = status;
+
+    }
+
+    // Getters e Setters.
+    public int numeroConta() {
+        return this.numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNomeDono() {
+        return this.nomeDono;
+    }
+
+    public void setNomeDono(String nomeDono) {
+        this.nomeDono = nomeDono;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    //Constructor abertura de conta.
+    public ContaBanco() {
+
+        status = false;
+
+        // Definindo status inicial da conta.
+        if (status == false) {
+            System.out.println("Conta fechada");
+        } else {
+            System.out.println("Conta aberta");
+        }
+
+        //Definindo bonificação de acordo com o tipo de conta.
+        if (tipo.equals("Conta Corrente")) {
+            this.saldo = 50;
+            System.out.println("Conta corrente recebeu bonificacao de abertura de R$ 50,00");
+        } else if (tipo.equals("Conta Poupanca")) {
+            this.saldo = 150;
+            System.out.println("Conta poupanca recebeu bonificacao de abertura de R$ 150,00");
+        }
+    }
+}
