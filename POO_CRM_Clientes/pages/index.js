b06eba80a36a1080a300e7ccc15bd8407962357d
Mikael S <150168560+SSMikael@users.noreply.github.com>
2025-06-05 16:30:28 +0000

    Alteração da fotne

--- POO_CRM_Clientes/pages/index.js
+++ POO_CRM_Clientes/pages/index.js
@@ -2,25 +2,35 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 export default function Home() {
-  return (
+ 
+    /* Header e Navegação */
+    return (
     <>
       <Head>
         <title>CRM FATEC - Pizzaria</title>
       </Head>
       
-      <header style={{ backgroundColor: '#383e42', color: 'white', padding: '40px', textAlign: 'left' }}>
+      <header style={{ 
+        backgroundColor: '#383e42', 
+        color: 'white', 
+        padding: '40px', 
+        textAlign: 'left', 
+        fontFamily: "bahnschrift", }}>
         <h1>CRM FATEC - Gestão de Clientes</h1>
       </header>
 
       <nav style={{ 
         backgroundColor: '#939597', 
-        padding: '10px', 
-        textAlign: 'center' }}
+        padding: '6px', 
+        textAlign: 'center',
+        fontSize:"20px",
+        /* textShadow: "2px 2px 4px #000000", (Sombreamento das letras)*/
+        fontFamily: "bahnschrift", }}
         >
 
         <Link href="/clientes" style={{ color: 'white', marginRight: '20px' }}>
             Clientes
-            </Link>
+            </Link> 
         <Link href="/pedidos" style={{ color: 'white', marginRight: '20px' }}>
             Pedidos
             </Link>
@@ -28,7 +38,7 @@ export default function Home() {
             Relatórios
             </Link>
       </nav>
-      
+
       <main style={{ padding: '20px' }}>
         <h2>Bem-vindo ao CRM da Pizzaria!</h2>
         <p>Gerencie seus clientes, pedidos e acompanhe relatórios em tempo real.</p>
