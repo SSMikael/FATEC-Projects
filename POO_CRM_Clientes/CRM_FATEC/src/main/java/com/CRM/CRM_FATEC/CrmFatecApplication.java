13f27d569546de20e49eac51b822afe8b5277088
Mikael S <150168560+SSMikael@users.noreply.github.com>
2025-06-06 03:18:42 +0000

    Índice

---
+++ POO_CRM_Clientes/CRM_FATEC/src/main/java/com/CRM/CRM_FATEC/CrmFatecApplication.java
@@ -0,0 +1,13 @@
+package com.CRM.CRM_FATEC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmFatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmFatecApplication.class, args);
+	}
+
+}
---
+++ POO_CRM_Clientes/CRM_FATEC/src/test/java/com/CRM/CRM_FATEC/CrmFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRM.CRM_FATEC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmFatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
