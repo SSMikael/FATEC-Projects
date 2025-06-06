a454cbd67e7149d4115eefc9b14f7926ca4c56e9
Mikael S <150168560+SSMikael@users.noreply.github.com>
2025-06-06 02:54:57 +0000

    Criação da classe e entidade Cliente
    Inclusão de comentários

---
+++ POO_CRM_Clientes/CRM_FATEC/src/main/java/com/CRM/CRM_FATEC/Cliente.java
@@ -0,0 +1,47 @@
+package com.CRM.CRM_FATEC;
+
+import jakarta.persistence.Entity;
+
+@Entity
+@table(name = "tb_clientes")
+
+
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private string nome;
+    private string cpf;
+    private Endereco endereco;
+    private int id;
+
+    public Cliente(string nome, string cpf) {
+
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public Cliente() {
+
+        this.nome = "";
+        this.cpf = "";
+    }
+
+    public string getNome() {
+        return nome;
+    }
+
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+
+    public string getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(string cpf) {
+        this.cpf = cpf;
+    }
+}
